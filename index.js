05ab0e0f1aae03da09e42311f58853fbd4558585
Paige Trask <paigetrask@gmail.com>
2022-06-06 07:27:17 -0700

    edited about page for better responsiveness, debugged mystery margin, ghost menu, nav scroll

--- index.js
+++ index.js
@@ -4,34 +4,58 @@ const aboutLink = document.querySelector("#about-link");
 const homeLink = document.querySelector("#home-link");
 const contactLink = document.querySelector("#contact-link");
 
+function stopTransition () {
+    const classes = document.body.classList;
+    let timer = 0;
+    window.addEventListener('resize', function () {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        } else 
+            classes.add('stop-transitions');
+
+        timer = setTimeout(() => {
+            classes.remove('stop-transitions');
+            timer = null;
+        }, 400);
+    
+    });
+};
+
+stopTransition()
+
+
 navToggle.addEventListener("click", () => {
     const visibility = navLinks.getAttribute('data-visible');
 
     if (visibility === "false") {
         navLinks.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
-        //document.body.classList.toggle('no-scroll');
+        document.body.classList.add("no-scroll");
     } else {
         navLinks.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
-        //document.body.classList.remove('no-scroll');
+        document.body.classList.remove("no-scroll");
     }
 });
-
 
 
 contactLink.addEventListener("click", () => {
     navLinks.setAttribute('data-visible', false)
     navToggle.setAttribute('aria-expanded', false)
+    document.body.classList.remove("no-scroll");
 })
 
 homeLink.addEventListener("click", () => {
     navLinks.setAttribute('data-visible', false)
     navToggle.setAttribute('aria-expanded', false)
+    document.body.classList.remove("no-scroll");
 })
 
 aboutLink.addEventListener("click", () => {
     navLinks.setAttribute('data-visible', false)
     navToggle.setAttribute('aria-expanded', false)
+    document.body.classList.remove("no-scroll");
 })
+
 
